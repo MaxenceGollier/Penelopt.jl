MaxenceGollier/Penelopt.jl

commit 865df209925dc7410344d17e732ebf2453a96410

match paper and update documentation

src/algorithm.jl
@@ -203,7 +203,13 @@ function SolverCore.solve!(
   ## R2N Specific arguments
   r2n_η1::T = √√eps(T),
   r2n_η2::T = isa(nlp, QuasiNewtonModel) ? T(0.9) : T(0.1),
-  r2n_γ::T = T(3),
+  r2n_η3::T = T(0.95),
+  r2n_γ0::T = sqrt(3),
+  r2n_γ1::T = T(9),
+  r2n_γ2::T = T(3),
+  r2n_γ3::T = T(1 / 3),
+  r2n_γ4::T = T(1 / 3),
+  r2n_γtr::T = T(1 / 3),
   r2n_watchdog_max_iter::Int = 10,
   r2n_watchdog_η0::T = √eps(T),
   r2n_tiny_step_tol::T = eps(T),
@@ -332,7 +338,13 @@ function SolverCore.solve!(
       σk = 1 / νsub,
       η1 = r2n_η1,
       η2 = r2n_η2,
-      γ = r2n_γ,
+      η3 = r2n_η3,
+      γ0 = r2n_γ0,
+      γ1 = r2n_γ1,
+      γ2 = r2n_γ2,
+      γ3 = r2n_γ3,
+      γ4 = r2n_γ4,
+      γtr = r2n_γtr,
       watchdog_max_iter = r2n_watchdog_max_iter,
       watchdog_η0 = r2n_watchdog_η0,
       tiny_step_tol = r2n_tiny_step_tol,

src/ir2n.jl
@@ -85,7 +85,12 @@ function SolverCore.solve!(
   η1::T = √√eps(T),
   η2::T = T(0.1),
   η3::T = T(0.95),
-  γ::T = T(3),
+  γ0::T = sqrt(3),
+  γ1::T = T(9),
+  γ2::T = T(3),
+  γ3::T = T(1 / 3),
+  γ4::T = T(1 / 3),
+  γtr::T = T(1 / 3),
   watchdog_max_iter::Int = 10,
   watchdog_η0::T = √eps(T),
   tiny_step_tol::T = eps(T),
@@ -197,7 +202,7 @@ function SolverCore.solve!(
     # Check the watchdog
     if check_watchdog!(watchdog_checkpoint, stats, mk, xk, watchdog_max_iter, watchdog_η0)
       fallback!(mk, xk, y, watchdog_checkpoint)
-      φ.data.σ *= γ^watchdog_max_iter
+      φ.data.σ *= γ2^watchdog_max_iter
       σk = φ.data.σ
       hk, fk = watchdog_checkpoint.hk, watchdog_checkpoint.fk
       m_fh_hist .= watchdog_checkpoint.m_fh_hist
@@ -248,19 +253,19 @@ function SolverCore.solve!(
     end
 
     if η2 ≤ ρk < Inf
-      σk = σk / γ
+      σk = σk * γ3
     end
 
     if η3 ≤ ρk < Inf
       if norm(y) <= ψ.h.lambda
-        σk = σk * clamp(sqrt(stats.dual_feas), 1 / γ, 1)
+        σk = σk * clamp(sqrt(stats.dual_feas), γ4, 1)
       end
 
     end
 
     if ρk < η1 || ρk == Inf
       if first_increase
-        σk = max(sqrt(stats.dual_feas), σk * γ)
+        σk = max(sqrt(stats.dual_feas), σk * γ2)
         first_increase = false
       elseif ρk < 0 && !is_active(watchdog_checkpoint) && !isa(nlp, NullHessianModel) # Watchdog procedure
 
@@ -283,17 +288,17 @@ function SolverCore.solve!(
           shift!(mk, xk, y = y)
 
         else
-          σk = σk * γ
+          σk = σk * γ2
         end
       else
         σms = more_sorensen_sigma!(
           solver.subsolver,
           solver.subpb,
           solver.substats;
-          Δ = norm(s) / γ,
+          Δ = norm(s) * γtr,
         )
-        σms = σk - σms
-        σk = clamp(σms, σk * sqrt(γ), σk * γ^2)
+        σms = σk + σms
+        σk = clamp(σms, σk * γ0, σk * γ1)
       end
     end
 

src/subsolvers/more-sorensen-sigma.jl
@@ -1,3 +1,10 @@
+# Compute a solution of
+#
+# (H+σI   Jᵀ )(s'(σ)) = (s)
+# (J     -αI )(y'(σ)) = (0)
+# 
+# And return the derivative of σ -> 1/‖s(σ)‖₂² - 1/Δ².
+# Used to update the quadratic regularization parameter σ.
 function more_sorensen_sigma!(
   solver::MoreSorensenSolver{T,V},
   reg_nlp::ShiftedL2PenalizedProblem{T,V,M,H,P},
@@ -10,11 +17,14 @@ function more_sorensen_sigma!(
   m = length(reg_nlp.h.b)
   solver_workspace = solver.workspace
 
-  @views @. solver.u2[1:n]         = -solver.x1[1:n]
+  # (H+σI   Jᵀ )(s'(σ)) = (s)
+  # (J     -αI )(y'(σ)) = (0)
+  @views @. solver.u2[1:n]         = solver.x1[1:n]
   @views @. solver.u2[(n+1):(n+m)] = 0
   solve_system!(solver_workspace, solver.u2) 
   get_solution!(solver.x2, solver_workspace)
 
+  # Compute derivative of σ -> 1/‖s(σ)‖₂² - 1/Δ²
   norm_x1 = norm(@view solver.x1[1:n])
 
   solver.u2 .= 0

commit 37d37dd4f6ba19f175e725a59d5e22864d0e954a

revert changes caused by 865df209925dc7410344d17e732ebf2453a96410

src/algorithm.jl
@@ -207,9 +207,9 @@ function SolverCore.solve!(
   r2n_γ0::T = sqrt(3),
   r2n_γ1::T = T(9),
   r2n_γ2::T = T(3),
-  r2n_γ3::T = T(1 / 3),
-  r2n_γ4::T = T(1 / 3),
-  r2n_γtr::T = T(1 / 3),
+  r2n_γ3::T = T(3),
+  r2n_γ4::T = T(3),
+  r2n_γtr::T = T(3),
   r2n_watchdog_max_iter::Int = 10,
   r2n_watchdog_η0::T = √eps(T),
   r2n_tiny_step_tol::T = eps(T),

src/ir2n.jl
@@ -88,9 +88,9 @@ function SolverCore.solve!(
   γ0::T = sqrt(3),
   γ1::T = T(9),
   γ2::T = T(3),
-  γ3::T = T(1 / 3),
-  γ4::T = T(1 / 3),
-  γtr::T = T(1 / 3),
+  γ3::T = T(3),
+  γ4::T = T(3),
+  γtr::T = T(3),
   watchdog_max_iter::Int = 10,
   watchdog_η0::T = √eps(T),
   tiny_step_tol::T = eps(T),
@@ -253,12 +253,12 @@ function SolverCore.solve!(
     end
 
     if η2 ≤ ρk < Inf
-      σk = σk * γ3
+      σk = σk / γ3
     end
 
     if η3 ≤ ρk < Inf
       if norm(y) <= ψ.h.lambda
-        σk = σk * clamp(sqrt(stats.dual_feas), γ4, 1)
+        σk = σk * clamp(sqrt(stats.dual_feas), 1 / γ4, 1)
       end
 
     end
@@ -295,7 +295,7 @@ function SolverCore.solve!(
           solver.subsolver,
           solver.subpb,
           solver.substats;
-          Δ = norm(s) * γtr,
+          Δ = norm(s) / γtr,
         )
         σms = σk + σms
         σk = clamp(σms, σk * γ0, σk * γ1)