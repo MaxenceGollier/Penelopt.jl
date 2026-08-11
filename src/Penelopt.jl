6a1635cf167b5fd3e08182f82591395ce002786a
MaxenceGollier <mgollier45@gmail.com>
2026-08-11 18:14:09 -0400

    add fixed variable treatment

--- src/Penelopt.jl
+++ src/Penelopt.jl
@@ -25,6 +25,8 @@ include("types/norm/NormL2.jl")
 include("types/norm/CompositeNormL2.jl")
 include("types/norm/ShiftedCompositeNormL2.jl")
 
+include("types/pre-processing/FixedVariable.jl")
+
 include("linear_algebra/K2.jl")
 include("linear_algebra/construct_workspace.jl")
 include("linear_algebra/ldlt.jl")
