d21cae3cd014f3a4300bc6c12dfabedcf157dbea
MaxenceGollier <mgollier45@gmail.com>
2026-08-11 16:13:01 -0400

    Update benchmarks to include custom filtering and allow problems with m == n

--- benchmark/benchmark-cutest-exact-hessian.jl
+++ benchmark/benchmark-cutest-exact-hessian.jl
@@ -9,6 +9,10 @@ problem_names = CUTEst.select_sif_problems(
   max_var = nmax,
   only_equ_con = true,
   only_free_var = true,
+  custom_filter = meta -> (
+    meta["variables"]["number"] >= meta["constraints"]["number"] # Uncomment to allow problems with fixed variables
+    #&& meta["variables"]["free"] + meta["variables"]["fixed"] == meta["variables"]["number"]
+  )
 )
 problem_list = (CUTEstModel(name) for name in problem_names)
 
@@ -28,5 +32,5 @@ solvers = Dict(
     ),
 )
 
-stats = bmark_solvers(solvers, problem_list, skipif = nlp -> nlp.meta.ncon ≥ nlp.meta.nvar)
+stats = bmark_solvers(solvers, problem_list)
 @save "benchmark/result/stats_exact.jld2" stats
