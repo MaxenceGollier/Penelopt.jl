d0151f27fe2408b5f83deea00cbc2a5cea3eb4ba
MaxenceGollier <mgollier45@gmail.com>
2026-08-17 10:29:29 -0400

    Benchmarks: allow for problems with fixed variables

--- benchmark/benchmark-cutest-lbfgs.jl
+++ benchmark/benchmark-cutest-lbfgs.jl
@@ -6,10 +6,9 @@ using CUTEst, Penelopt, SolverBenchmark
 problem_names = CUTEst.select_sif_problems(
   min_con = 1,
   only_equ_con = true,
-  only_free_var = true,
   custom_filter = meta -> (
-    meta["variables"]["number"] >= meta["constraints"]["number"] # Uncomment to allow problems with fixed variables
-    #&& meta["variables"]["free"] + meta["variables"]["fixed"] == meta["variables"]["number"]
+    meta["variables"]["number"] >= meta["constraints"]["number"]
+    && meta["variables"]["free"] + meta["variables"]["fixed"] == meta["variables"]["number"]
   ),
 )
 
@@ -44,7 +43,7 @@ solvers = Dict(
       rtol = 0.0,
       max_time = max_time,
       max_iter = typemax(Int),
-      hessian_approximation = "limited-memory",
+      qn_hessian_approximation = "limited-memory",
       linear_solver = "mumps",
     ),
 )
