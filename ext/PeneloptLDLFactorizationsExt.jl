50cc052b34349d642043a85059564940ee5d76da
MaxenceGollier <mgollier45@gmail.com>
2026-09-01 12:10:27 -0400

    use blas in LDLFactorizations

--- ext/PeneloptLDLFactorizationsExt.jl
+++ ext/PeneloptLDLFactorizationsExt.jl
@@ -5,9 +5,14 @@ using Penelopt
 
 using LinearAlgebra, SparseArrays, SparseMatricesCOO
 
+# Import BLAS functions
+import LinearAlgebra.BLAS: @blasfunc
+import LinearAlgebra: BlasInt, libblastrampoline
+
 import Penelopt: AbstractLDLTWorkspace
 import Penelopt: construct_ldlt_workspace, solve_system!, update_workspace!
 import Penelopt: get_inertia, get_solution!, get_status
+import Penelopt: getrf!, getrs!
 import Penelopt: set_dual_inertia!, set_primal_inertia!
 
 include("LDLFactorizations/ldlt.jl")
